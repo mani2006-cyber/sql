2ffbb691e775b367069f53a4abe2992ec8fc2c20
mani2006-cyber <manikantagedda2006@gmail.com>
2025-04-15 11:01:16 +0000

    add new fil

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template, request, url_for
-
+from flask import Flask, render_template, request, url_for,jsonify
 app = Flask(__name__)
 
+content = [{
+    'name': 'John Doe',
+    'email': 'john.doe@example.com',
+    'phone': '123-456-7890',
+    'address': 's. chintamani ,nagar, hyderabad, telangana, 500049'
+}]
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html' ,content = content)
 
-
+@app.route('/jobs')
+def jobs():
+    return jsonify(content)
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
